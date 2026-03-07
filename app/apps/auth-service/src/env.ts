51ce8e5193f344d2508adcf1186ee627ae27de25
Karinacho <kari.cholakova@gmail.com>
2026-03-07 21:53:18 +0200

    fix: env small fixes

--- app/apps/auth-service/src/env.ts
+++ app/apps/auth-service/src/env.ts
@@ -8,7 +8,7 @@ const stage = process.env.APP_STAGE ?? 'dev';
 const result = dotenv.config({ path: `.env.${stage}` });
 
 if (result.error) {
-  dotenv.config({ path: `.env}` });
+  dotenv.config({ path: `.env` });
 }
 
 
@@ -34,7 +34,7 @@ try {
 
     e.issues.forEach(err => {
       const path = err.path.join('.')
-      console.log(`[${path}: ${err.message}`);
+      console.log(`[${path}]: ${err.message}`);
     })
 
     process.exit(1);
